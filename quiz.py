45bde40dfe761cad3851ef4a19d500ad2c1d9828
idk-aarza <aarzak2012@gmail.com>
2024-10-19 11:46:09 +0530

    mandala

---
+++ quiz.py
@@ -0,0 +1,18 @@
+question=input('What is the age of Aarza')
+if question == 12:
+    print('Correct Answer')
+else:
+    print('Incorrect answer the answer is 12')
+    
+    
+    #Profit
+costprice= int(input('enter the cost price'))
+sellingprice= int(input('enter the selling price'))
+if(sellingprice > costprice) :
+    print('profit')
+    pt=sellingprice - costprice
+    print(pt)
+else:
+    print('no profit') 
+    
+
