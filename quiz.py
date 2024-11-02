801251cee141ccde078c3088b5cafbaa9b09fced
idk-aarza <aarzak2012@gmail.com>
2024-11-02 10:03:00 +0530

    odde

--- quiz.py
+++ quiz.py
@@ -15,4 +15,11 @@ if(sellingprice > costprice) :
 else:
     print('no profit') 
     
+number=int(input("Enter a no. to check"))
+print('no. to be checked:', number)
+if number%2==0:
+    print('This is an even no.')
+else:
+    print('This is an odd no.')
+    
 
